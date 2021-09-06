65d00170c15637d0d1a41107ebe7d0a6b44b2422
Dalilah Aguilar <dalilahaguilar9@gmail.com>
2021-09-05 21:40:32 -0500

    commit tree 317b1d96a545d6c9b932de72a07ce3918034acaa

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Your code here
+function saturdayFun(skate = 'roller-skate'){
+   return  `This Saturday, I want to ${skate}!`
+}
+
+const mondayWork = function (string = 'go to the office'){
+    return `This Monday, I will ${string}.` 
+}
+
+
+
+
+
+function wrapAdjective(msg = 'You are'){
+    return function (highlight, highlight2 = '*') {
+    return `${msg} ${highlight} ${highlight2}!`
+    }
+}
+wrapAdjective()("*");
